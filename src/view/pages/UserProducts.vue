4131253bf959fea9efe40c4e437e1c07b184efd2
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2023-10-10 16:05:37 +0100

    Update UserProducts.vue

--- src/view/pages/UserProducts.vue
+++ src/view/pages/UserProducts.vue
@@ -7,14 +7,14 @@
     >
     </sdPageHeader>
 
-    <Main>
+    <Main v-if="profile.userRole === 'customer'">
       <div class="flex justify-between my-6">
         <input
           class="max-w-[300px] w-full border bordergray-300 rounded-lg px-3 py-2"
           placeholder="Search description"
           v-model="search"
         />
-        <div>
+        <div v-if="profile.userRole === 'customer'">
           <router-link to="/service/request">
             <sdButton
               @click="visible = true"
@@ -23,7 +23,7 @@
               key="1"
               type="primary"
             >
-           Request service
+              Request service
             </sdButton>
           </router-link>
         </div>
@@ -36,7 +36,13 @@
 <script setup>
 import { Main } from "../styled";
 import Products from "components/pageComponents/userproducts/overview/ProductsTable";
-import { ref, provide } from "vue";
+import { ref, provide, computed, onBeforeMount } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const { state } = useStore();
+const profile = computed(() => state.auth.userData);
 const breadcrumbs = [
   {
     path: "/dashboard",
@@ -48,5 +54,11 @@ const breadcrumbs = [
   },
 ];
 const search = ref("");
+
+onBeforeMount(() => {
+  if (profile.value.userRole !== "customer") {
+    router.push("/");
+  }
+});
 provide("search", search);
 </script>
